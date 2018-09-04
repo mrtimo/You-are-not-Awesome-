32744d0bcf7aa9cb5fa369611cb97310178cb5c7
Tim Olsen <olsen.tim@gmail.com>
2018-09-04 13:24:05 -0700

    Initial Commit

---
+++ You are not Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You are not Awesome!
+//
+//  Created by Tim Olsen on 9/4/18.
+//  Copyright © 2018 Tim Olsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
